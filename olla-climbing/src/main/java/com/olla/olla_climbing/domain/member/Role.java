a4f7d2df8d6d5632ae71cd190944af476fe1c38b
kwoo618 <kangwoo111@naver.com>
2026-02-10 15:27:17 +0900

    Feat: MemberRepository 및 Role Enum 생성

--- olla-climbing/src/main/java/com/olla/olla_climbing/domain/member/Role.java
+++ olla-climbing/src/main/java/com/olla/olla_climbing/domain/member/Role.java
@@ -1,5 +1,13 @@
 package com.olla.olla_climbing.domain.member;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor    // final로 선언된 필드를 매개변수로 받는 생성자를 자동 생성
 public enum Role {
-    USER, ADMIN
+    USER("ROLE_USER","일반 사용자"), ADMIN("ROLE_ADMIN","관리자");
+
+    private final String key;
+    private final String title;
 }
