fbbd458a43031fc8d38d118d6dc221fc3fce903e
io8272 <io8272@naver.com>
2026-04-08 12:04:35 +0900

    리액트 세팅 완료 gitignore도 세팅 완료 바로 작업하면됨

---
+++ olla_frontend/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
+      <LoginScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
