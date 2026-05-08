011181f6f55fefb84bd29d014deeaab9c9335256
lemonmuffin <dh8483@naver.com>
2026-05-08 22:50:32 +0900

    시작시 로고 화면 구현

--- olla_frontend/LoginScreen.tsx
+++ olla_frontend/LoginScreen.tsx
@@ -16,7 +16,7 @@ const LoginScreen = ({ navigation }: any) => {
     }
 
     try {
-      const response = await axios.post('http://192.168.0.23:8080/api/v1/auth/login', {
+      const response = await axios.post('http://10.0.2.2:8080/api/v1/auth/login', {
         loginId: loginId,
         password: password,
       });
