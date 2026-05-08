ff1b340c8e2a12f6d8be1c025adfbdf1c352280e
lemonmuffin <dh8483@naver.com>
2026-05-09 00:02:14 +0900

    오류수정진행중

--- olla_frontend/LoginScreen.tsx
+++ olla_frontend/LoginScreen.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = ({ navigation }: any) => {
   const [loginId, setLoginId] = useState('');
